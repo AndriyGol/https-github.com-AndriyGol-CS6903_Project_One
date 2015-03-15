b10f5a44864df30e4b683927f09afb6a4536e266
AndriyGoltsev <goltsev.andriy@gmail.com>
2015-03-15 12:20:10 -0400

    First fully working version

--- project1/project1/main.cpp
+++ project1/project1/main.cpp
@@ -15,30 +15,39 @@
 #include <stdlib.h>     
 #include <time.h>
 #include <algorithm>
+#include <thread> 
+#include <ios>
+#include <mutex>
+#include <unordered_set>
+#include "dictionaries.cpp"
+
 using namespace std;
 
 #define ALHPABET_SIZE 27
-#define KEY_ALHPABET_SIZE 26
+#define KEY_ALHPABET_SIZE 27
 //#define CIPHER_TEXT_ALHPABET_SIZE KEY_ALHPABET_SIZE
 #define TEXT_ALPHA_START 64
 #define CIPHER_ALPHA_START 65
-#define KEY_SIZE 7
+#define KEY_SIZE 10
+#define MAX_THREADS 40
+#define CIPHER_TEXT_SIZE 100
 
 vector<string> dictionary1;
 vector<string> dictionary2;
+vector<string> plainTextCandidates;
 
 vector<string> roots;
-int keySize;
-set<int> _globalSet;
+//int keySize;
+mutex mtx;
 
 //unsigned int getShiftNumber(char cipher, char plain);
 
-int aton(char c) {
+int aton(int c) {
     if (c == ' '){
         return 0;
     }
     else {
-        return c - 'A' + 1;
+        return c - 'a' + 1;
     }
 }
 
@@ -47,7 +56,7 @@ char ntoa(int n) {
         return ' ';
     }
     else {
-        return n + 'A' - 1;
+        return n + 'a' - 1;
     }
 }
 
@@ -61,45 +70,42 @@ int getShiftNumber(char cipher, char plain) {
 }
 
 char shiftBy(char ch, int rot) {
-    return ntoa((aton(ch)+rot)%ALHPABET_SIZE);
+    return ntoa((aton(ch) + rot) % ALHPABET_SIZE);
 }
 
-bool readDictionary1() {
-    string fileName = "/Users/AndriyGoltsev/git/CryptProject1/project1/project1/Dictionary1.txt";
-    string line;
-    ifstream myfile (fileName.c_str());
-    if (myfile.is_open()){
-        
-        while ( getline (myfile,line) ) {
-            std::transform(line.begin(), line.end(), line.begin(), ::toupper);
-            dictionary1.push_back(line);
-        }
-        myfile.close();
-        cout << "Read Dictionary 1 of size " << dictionary1.size() << " lines.\n";
-        
-        return true;
+vector<int> generateRandomKey(int keyLen) {
+    // generate key
+    vector<int> key;
+    string stringKey;
+    stringstream ss;
+    int rot;
+    for (int i = 0; i < keyLen; i++) {
+        rot = rand() % KEY_ALHPABET_SIZE;
+        key.push_back(rot);
     }
-    else {
-        cerr << "Unable to open file " + fileName;
-        return false;
-    }
+
+#ifdef VERBOSE_MODE
+    sort(key.begin(), key.end());
+    for (int& k : key) { ss << k << ' '; }
+    cout << "Key : " << ss.str() << '\n';
+#endif
     
-    return false;
+    return key;
 }
 
-bool readDictionary2() {
-    string fileName = "/Users/AndriyGoltsev/git/CryptProject1/project1/project1/Dictionary2.txt";
-    string line;
-    ifstream myfile (fileName.c_str());
-    if (myfile.is_open()){
-        
-        while ( getline (myfile,line) ) {
-            std::transform(line.begin(), line.end(), line.begin(), ::toupper);
-            line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
-            dictionary2.push_back(line);
-        }
-        myfile.close();
-        
+void addToKeyCandidates(string s) {
+    mtx.lock();
+    plainTextCandidates.push_back(s);
+    mtx.unlock();
+}
+
+void readDictionary1() {
+    dictionary1 = getDictionary1();
+}
+
+void readDictionary2() {
+    
+        dictionary2 = getDictionary2();
         size_t dictionarySize = dictionary2.size();
         roots.reserve(dictionarySize * dictionarySize * dictionarySize);
         
@@ -108,103 +114,69 @@ bool readDictionary2() {
                 for(size_t k = 0; k < dictionarySize; k++){
                     roots.push_back(dictionary2[i] + " " + dictionary2[j] + " " + dictionary2[k]);
                 }
-        
-        cout << "Read Dictionary 2 of size " << dictionary2.size() << " lines.\n";
-        
-        return true;
-    }
-    else {
-        cerr << "Unable to open file " + fileName;
-        return false;
-    }
+}
+
+string encrypt(string plainText, vector<int> key) {
     
-    return false;
+    cout << "Plain text: " << plainText << '\n';
+#ifdef VERBOSE_MODE
+    cout << "******* Crypto Map ********\n";
+#endif
+    
+    int c, p, k;
+    string cipher;
+    for (int i = 0; i < plainText.size(); i++) {
+        p = aton(plainText[i]);
+        k = rand() % key.size();
+        c = ntoa((p + key[k]) % ALHPABET_SIZE );
+#ifdef VERBOSE_MODE
+        cout << "C(" << plainText[i] << " , " << key[k] << ")" << "\t=>\t" << c << " (" << (char)c  << ")\n";
+#endif
+        cipher.push_back(c);
+        
+    }
+#ifdef VERBOSE_MODE
+    cout << "******* Crypto Map END ********\n";
+#endif
+    return cipher;
+}
+
+void trimToSize(string& text) {
+    if (text.size() > CIPHER_TEXT_SIZE)
+        text.resize(CIPHER_TEXT_SIZE);
 }
 
 string encryptD1(int keyLen) {
     
     // generate key
-    vector<int> key;
-    string stringKey;
-    stringstream ss;
-    int rot;
-    for (int i = 0; i < keyLen; i++) {
-        rot = 1 + rand() % KEY_ALHPABET_SIZE;
-        key.push_back(rot);
-        ss << rot << ' ';
-    }
+    vector<int> key =generateRandomKey(keyLen);
     
     string plainText = dictionary1[rand() % dictionary1.size()];
-    string cipher;
-    
-    int c, p, k;
-    
-    cout << "Plain text: " << plainText << '\n';
-    cout << "Key : " << ss.str() << '\n';
-    cout << "Cipher text : " << cipher << '\n';
-    cout << "******* Crypto Map ********\n";
-    
-    for (int i = 0; i < plainText.size(); i++) {
-        p = aton(plainText[i]);
-        k = rand() % key.size();
-        c = ntoa((p + key[k]) % ALHPABET_SIZE );
-        cout << "C(" << plainText[i] << " , " << key[k] << ")" << "\t=>\t" << c << " (" << (char)c  << ")\n";
-        
-        cipher.push_back(c);
-        
-    }
+    string cipher = encrypt(plainText, key);
 
     return cipher;
 }
 
-string encryptD2(int keyLen, int maxSize) {
+string encryptD2(int keyLen) {
     
     // generate key
-    vector<int> key;
-    string stringKey;
-    stringstream ss;
-    int rot;
-    for (int i = 0; i < keyLen; i++) {
-        rot = 1 + rand() % KEY_ALHPABET_SIZE;
-        key.push_back(rot);
-        ss << rot << ' ';
-    }
-    
+    vector<int> key = generateRandomKey(keyLen);
     string plainText;
     string cipher;
     
-    while (plainText.size() != maxSize) { // TODO ask if L is always 100!!!
-        plainText = dictionary2[rand() % dictionary2.size()];
-        while (plainText.size() < maxSize) {
-            plainText += " " + dictionary2[rand() % dictionary2.size()];
-        }
-        cout << 'x';
+    plainText = dictionary2[rand() % dictionary2.size()];
+    while (plainText.size() < CIPHER_TEXT_SIZE) {
+        plainText += " " + dictionary2[rand() % dictionary2.size()];
     }
     
-    
-    int c, p, k;
-    cout << "Plain text: " << plainText << '\n';
-    cout << "Key : " << ss.str() << '\n';
-    cout << "Cipher text : " << cipher << '\n';
-    cout << "******* Crypto Map ********\n";
-    
-    for (int i = 0; i < plainText.size(); i++) {
-        
-        p = aton(plainText[i]);
-        k = rand() % key.size();
-        c = ntoa((p + key[k]) % ALHPABET_SIZE );
-        cout << "C(" << plainText[i] << " , " << key[k] << ")" << "\t=>\t" << c << " (" << (char)c  << ")\n";
-        
-        cipher.push_back(c);
-        
-    }
+    trimToSize(plainText);
+    cipher = encrypt(plainText, key);
     
     return cipher;
 }
 
-void decryptD1() {
+void decryptD1(int keySize, string cipherText) {
     
-    string cipherText = encryptD1(keySize);
     
     vector<string>::const_iterator it = dictionary1.begin();
     string line;
@@ -219,9 +191,7 @@ void decryptD1() {
         }
         
         if (keySet.size() <= keySize) {
-            cout << '\n';
-            cout << line;
-            cout << '\n';
+            addToKeyCandidates(line);
         }
         
         it++;
@@ -229,20 +199,16 @@ void decryptD1() {
 
 }
 
-int fitsKeyConstraint(const string& line, const string& cipherText) {
-    
-    if(line.size() > cipherText.size()) {
-        return -1;
-    }
+int fitsKeyConstraint(const string& line, const string& cipherText, int myKeySize ) {
 
-    _globalSet.clear();
+    unordered_set<int> keySet;
     int shift;
     for (size_t i = 0; i < line.size(); i++) {
         shift = getShiftNumber(cipherText[i] ,line[i]);
-        _globalSet.insert(shift);
+        keySet.insert(shift);
     }
     
-    if (_globalSet.size() <= keySize) {
+    if (keySet.size() <= myKeySize) {
         return line.size() == cipherText.size() ? 0 : 1;
     }
     else {
@@ -250,53 +216,102 @@ int fitsKeyConstraint(const string& line, const string& cipherText) {
     }
 }
 
-void decryptRecursive(string line, const string& cipherText) {
-    int status = fitsKeyConstraint(line, cipherText);
+void decryptRecursive(string line, const string& cipherText, int myKeySize) {
+    
+    trimToSize(line);
+    int status = fitsKeyConstraint(line, cipherText, myKeySize);
     
     if (status == 0) {
-        cout << line << '\n'; // candidate found!!!
+        addToKeyCandidates(line);
     }
-    else if ( status > 0 ) {
+    else if ( status > 0 && line.size() < cipherText.size()) {
         for (int i = 0; i < dictionary2.size(); i++) {
-            decryptRecursive(line + " " + dictionary2[i] , cipherText);
+            decryptRecursive(line + " " + dictionary2[i] , cipherText, myKeySize);
         }
     }
 }
 
-void decryptD2() {
+void decryptThread(size_t start, size_t end , const string& cipherText, int keySize) {
     
-    string cipherText = encryptD2(KEY_SIZE, 100);
-    
-    vector<string>::const_iterator it = roots.begin();
-    string line;
-    while (it != roots.end()) {
-        line = *it;
-        
-        decryptRecursive(line, cipherText);
-        
-        it++;
+    for (size_t i = start; i < end; i++) {
+            decryptRecursive(roots[i], cipherText, keySize);
     }
     
 }
 
+void decryptD2(int keySize, string cipherText) {
+    
+    size_t chunkSize = roots.size() / MAX_THREADS;
+        
+    size_t start, end;
+    vector<thread> threads;
+    for (size_t i = 0;  i <= MAX_THREADS; i++) {
+        
+            start = i * chunkSize;
+            end = (i + 1) * chunkSize;
+        
+            if (start < roots.size()) {
+            
+                if (end > roots.size()) {
+                    end  = roots.size();
+                }
+            
+                threads.push_back(thread(decryptThread, start, end, cipherText, keySize));
+            }
+    }
+        
+    for (auto& th : threads) th.join();
+    
+    return;
+}
+
 int main (int argc, char** argv) {
     srand(time(NULL));
-    
     string cipherText;
-    
+    int keySize;
     
     readDictionary1();
     readDictionary2();
     
-    if(argc == 3) {
-        keySize = atoi(argv[1]);
-        cipherText = argv[2];
-    }
-    else {
-        keySize = KEY_SIZE; // TODO move to main
-        //decryptD1();
-        decryptD2();
-    }
+//    if(argc == 3) {
+//        keySize = atoi(argv[1]);
+//        cipherText = argv[2];
+//    }
+//    else {
+//        keySize = KEY_SIZE; // TODO move to main
+//        decryptD1(keySize, encryptD1(keySize));
+//        cout << "Guess: " << plainTextCandidates[rand()%plainTextCandidates.size()] << endl;
+//        
+//        plainTextCandidates.clear();
+//        decryptD2(keySize, encryptD2(keySize));
+//        cout << "Guess: " << plainTextCandidates[rand()%plainTextCandidates.size()] << endl;
+//    }
+    
+    
+    
+    do {
+        plainTextCandidates.clear();
+        
+        cout << "Enter key size: ";
+        cin >> keySize;
+        cout << "Enter cipher text: ";
+        getline(cin, cipherText, '\n');
+        
+        decryptD1(keySize, cipherText);
+        
+        if(plainTextCandidates.empty()) {
+            decryptD2(keySize, cipherText);
+        }
+        
+        if(plainTextCandidates.empty()) {
+            cout << "Could not decrypt " << cipherText << endl;
+            cout << "Please try again." << endl;
+        }
+        else {
+            cout << "Guess: " << plainTextCandidates[rand()%plainTextCandidates.size()] << endl;
+        }
+        
+    } while (true);
     
     return 0;
 }
